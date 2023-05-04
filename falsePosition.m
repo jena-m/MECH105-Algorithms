43c886769ffa69773b821dff224c707aecffa860
jena-m <132479218+jena-m@users.noreply.github.com>
2023-05-04 14:04:00 -0600

    Update falsePosition.m

--- falsePosition.m
+++ falsePosition.m
@@ -1,5 +1,17 @@
 function [root, fx, ea, iter] = falsePosition(func, xl, xu, es, maxit, varargin)
-%falsePosition finds the root of a function using false position method
+% This function performs linear regression using the false position method to find the root of a function within a given interval.
+% Inputs:
+%   func - the function to be evaluated
+%   xl - the lower bound of the interval
+%   xu - the upper bound of the interval
+%   es (optional) - the desired relative error (default value: 0.0001)
+%   maxit (optional) - the maximum number of iterations (default value: 200)
+%   varargin - additional arguments to be passed to the function
+% Outputs:
+%   root - the approximate root of the function
+%   fx - the function value at the root
+%   ea - the approximate relative error
+%   iter - the number of iterations performed
 
 if nargin < 3
     error('You must have at least 3 inputs, func, xl, xu')
